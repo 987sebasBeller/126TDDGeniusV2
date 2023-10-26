58e8fc912a05ed11166c22ff7654d5aeb66a14e2
987sebasBeller <sebastianbeller76@gmail.com>
2023-10-26 09:42:47 -0400

    Devuelve el estado del ejercicio de terminado a no Terminado

--- src/Ejercicio.js
+++ src/Ejercicio.js
@@ -33,8 +33,9 @@ class Ejercicio{
     validarAtributos(){
         return this.validarTitulo() && this.validarCategoria() && this.validarResumen();
     }
-    setEstado(){
-        return "Terminado";
+    setEstado(estadoNuevo){
+        this.estado=estadoNuevo;
+        
     }
 
 }
--- src/spec/editarEstadoEjer.spec.js
+++ src/spec/editarEstadoEjer.spec.js
@@ -2,7 +2,14 @@ import { Ejercicio } from "../Ejercicio";
 describe("Editar estado ejercicio", () => {
     it("Devuelve el estado del ejercicio de No terminado a Terminado", () => {
       let ejercicio=new Ejercicio("ejer1","c1",undefined,"resumen1","No Terminado");
-        expect(ejercicio.setEstado("Terminado")).toEqual("Terminado");
+      ejercicio.setEstado("Terminado");
+        expect(ejercicio.getEstado()).toEqual("Terminado");
       });
+      it("Devuelve el estado del ejercicio de terminado a no Terminado", () => {
+        let ejercicio=new Ejercicio("ejer1","c1",undefined,"resumen1","Terminado");
+      ejercicio.setEstado("No Terminado");
+
+          expect(ejercicio.getEstado()).toEqual("No Terminado");
+        });
   }
 );
