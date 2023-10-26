72139773af6915db1cb8ac24e1ba7fc0c004d5d8
987sebasBeller <sebastianbeller76@gmail.com>
2023-10-26 09:07:46 -0400

    Refactoring pruebas buscporestado

--- src/spec/buscarPorEstado.spec.js
+++ src/spec/buscarPorEstado.spec.js
@@ -1,25 +1,31 @@
 import { Catalogo } from "../Catalogo";
 import { Ejercicio } from "../Ejercicio";
 import { ejerciciosPruebas } from "../constantes";
+function getEstado(estado){
+    let ejercicio=new Ejercicio("ejer1","c1",undefined,"resumen1",estado);
+    return ejercicio.getEstado();
+
+}
 describe("Devolver estado del ejercicio", () => {
     it("Devuelve No Terminado ejercicio no terminado", () => {
-        let ejercicio=new Ejercicio("ejer1","c1",undefined,"resumen1")
-        expect(ejercicio.getEstado()).toEqual("No Terminado");
+        expect(getEstado("No Terminado")).toEqual("No Terminado");
       });
       it("Devuelve Terminado ejercicio Terminado", () => {
-        let ejercicio=new Ejercicio("ejer1","c1",undefined,"resumen1","Terminado")
-        expect(ejercicio.getEstado()).toEqual("Terminado");
+        expect(getEstado("Terminado")).toEqual("Terminado");
       });
   }
 );
+function buscarPorEstado(ejercicios,estado){
+    let catalogo=new Catalogo(ejercicios);
+    return catalogo.buscarPorEstado(estado);
+
+}
 describe("Buscar ejercicio por estado", () => {
     it("Devuelve los ejercicios con estado termniado", () => {
-        let catalogo=new Catalogo(ejerciciosPruebas);
-        expect(catalogo.buscarPorEstado("Terminado")).toEqual([{"categoria": "Numeros","estado": "Terminado", "imagen": "1euc7nmD5AGroYcxYYx4DEd-MYAzXuUph", "resumen": "resumen", "titulo": "Numeros Primos"}]);
+        expect(buscarPorEstado(ejerciciosPruebas,"Terminado")).toEqual([{"categoria": "Numeros","estado": "Terminado", "imagen": "1euc7nmD5AGroYcxYYx4DEd-MYAzXuUph", "resumen": "resumen", "titulo": "Numeros Primos"}]);
       });
       it("Devuelve los ejercicios con estado no termniado", () => {
-        let catalogo=new Catalogo(ejerciciosPruebas);
-        expect(catalogo.buscarPorEstado("No Terminado")).toEqual(ejerciciosPruebas.filter(ele=>ele.getEstado()=="No Terminado"));
+        expect(buscarPorEstado(ejerciciosPruebas,"No Terminado")).toEqual(ejerciciosPruebas.filter(ele=>ele.getEstado()=="No Terminado"));
       });
   }
 );
