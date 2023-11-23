c6fded0298b5587e5c84caeadf8fd559de6e0de0
Wama85 <wi_an9@hotmail.com>
2023-11-22 22:35:15 -0400

    Verificar que hubo cambios en la modificación de ejercicios

--- src/presenterEditarEjercicio.js
+++ src/presenterEditarEjercicio.js
@@ -5,10 +5,18 @@ const ejercicio=catalogo.buscarPorNombre(valorTitulo)[0];
 const detalleCatalogo = document.getElementById("contenido");
 const categoria = document.getElementById("selecCategoria");
 const detalle = document.getElementById("txtDetalle");
-
+const btnEditar=document.getElementById("editarregistro");
 document.getElementById("txtNombre").value=valorTitulo;
 detalleCatalogo.style.display="none";
 categoria.value=ejercicio.getCategoria();
 detalle.innerHTML=ejercicio.getResumen();
+btnEditar.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const titulo=document.getElementById("txtNombre");
+    ejercicio.setTitulo(titulo.value);
+    ejercicio.setCategoria(categoria.value);
+    ejercicio.setResumen(detalle.value);
+    catalogo.guardarEjercicios();
 
+});
 console.log(detalle)
