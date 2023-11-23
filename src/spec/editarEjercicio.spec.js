3da70e67ed3e0b3b6a5af8283a823cbe98b46ade
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-11-23 16:56:20 -0400

    Se refactoriza Editar ejercicio en el catalogo

--- src/spec/editarEjercicio.spec.js
+++ src/spec/editarEjercicio.spec.js
@@ -2,48 +2,51 @@ import { ejerciciosPruebas } from "../constantes";
 import {Ejercicio} from "../models/Ejercicio";
 import { Catalogo } from "../models/Catalogo.js";
 import "jest-localstorage-mock";
-function getTituloModificado(posicionEjercicio,nuevoTitulo){
-  let ejercicio=ejerciciosPruebas[posicionEjercicio];
+function getTituloModificado(ejercicioPrueba,nuevoTitulo){
+  let ejercicio=ejercicioPrueba;
   ejercicio.setTitulo(nuevoTitulo); 
   return ejercicio.getTitulo();
 }
-function getCategoriaModificado(posicionEjercicio,nuevoCategoria){
-  let ejercicio=ejerciciosPruebas[posicionEjercicio];
+function getCategoriaModificado(ejercicioPrueba,nuevoCategoria){
+  let ejercicio=ejercicioPrueba;
   ejercicio.setCategoria(nuevoCategoria); 
   return ejercicio.getCategoria();
 }
-function getResumenModificado(posicionEjercicio,nuevoResumen){
-  let ejercicio=ejerciciosPruebas[posicionEjercicio];
+function getResumenModificado(ejercicioPrueba,nuevoResumen){
+  let ejercicio=ejercicioPrueba;
   ejercicio.setResumen(nuevoResumen); 
   return ejercicio.getResumen();
 }
 describe("Editar Ejercicio", () => {
 
     it("Permitir modificar el título", () => {
-      expect(getTituloModificado(1,"Numeros Romanos X")).toEqual('Numeros Romanos X');
+      expect(getTituloModificado(ejerciciosPruebas[1],"Numeros Romanos X")).toEqual('Numeros Romanos X');
     });
     it("Permitir modificar cualquier título", () => {
-        expect(getTituloModificado(1,"Numeros Romanos 10")).toEqual('Numeros Romanos 10');
+        expect(getTituloModificado(ejerciciosPruebas[1],"Numeros Romanos 10")).toEqual('Numeros Romanos 10');
       });
     it("Permitir modificar la categoría", () => {
-        expect(getCategoriaModificado(0,"Numeros")).toEqual('Numeros');
+        expect(getCategoriaModificado(ejerciciosPruebas[0],"Numeros")).toEqual('Numeros');
       });
       it("Permitir modificar cualquier categoría", () => {
-        expect(getCategoriaModificado(2,"Arboles")).toEqual('Arboles');
+        expect(getCategoriaModificado(ejerciciosPruebas[2],"Arboles")).toEqual('Arboles');
       });
       it("Permitir modificar cualquier resumen", () => {
-        expect(getResumenModificado(1,"resumen4")).toEqual('resumen4');
+        expect(getResumenModificado(ejerciciosPruebas[1],"resumen4")).toEqual('resumen4');
       });
   }
 );
+function buscarEjercicioPorNombreEnCatalogo(ejerciciosPruebas,nombre){
+  let catalogo=new Catalogo(ejerciciosPruebas);
+  let ejercicio=catalogo.buscarPorNombre(nombre)[0];
+  return ejercicio;
+}
 
 describe("Editar Ejercicio del Catálogo", () => {
 
     it("Edita el titulo del ejercicio numero primos", () => {
-      let catalogo=new Catalogo(ejerciciosPruebas);
-      let ejercicio=catalogo.buscarPorNombre("Numeros Primos")[0];
-      ejercicio.setTitulo('Numeros Romanos 10');
-      expect(ejercicio.getTitulo()).toEqual('Numeros Romanos 10');
+      let ejercicio=buscarEjercicioPorNombreEnCatalogo(ejerciciosPruebas,"Numeros Primos")
+      expect(getTituloModificado(ejercicio,"Numeros Romanos 10")).toEqual('Numeros Romanos 10');
     });
  
     
