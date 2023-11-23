99a63e9a6b96da986bb096e91b52774d694f000e
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-11-23 16:38:24 -0400

    Refactring pruebas editarEjercicio se crea getTotuloModificado

--- src/spec/editarEjercicio.spec.js
+++ src/spec/editarEjercicio.spec.js
@@ -2,17 +2,18 @@ import { ejerciciosPruebas } from "../constantes";
 import {Ejercicio} from "../models/Ejercicio";
 import { Catalogo } from "../models/Catalogo.js";
 import "jest-localstorage-mock";
+function getTituloModificado(posicionEjercicio,nuevoTitulo){
+  let ejercicio=ejerciciosPruebas[posicionEjercicio];
+  ejercicio.setTitulo(nuevoTitulo); 
+  return ejercicio.getTitulo();
+}
 describe("Editar Ejercicio", () => {
 
     it("Permitir modificar el título", () => {
-      let ejercicio=ejerciciosPruebas[1];
-      ejercicio.setTitulo('Numeros Romanos X');
-      expect(ejercicio.getTitulo()).toEqual('Numeros Romanos X');
+      expect(getTituloModificado(1,"Numeros Romanos X")).toEqual('Numeros Romanos X');
     });
     it("Permitir modificar cualquier título", () => {
-        let ejercicio=ejerciciosPruebas[1];
-        ejercicio.setTitulo('Numeros Romanos 10');
-        expect(ejercicio.getTitulo()).toEqual('Numeros Romanos 10');
+        expect(getTituloModificado(1,"Numeros Romanos 10")).toEqual('Numeros Romanos 10');
       });
     it("Permitir modificar la categoría", () => {
         let ejercicio=ejerciciosPruebas[0];
