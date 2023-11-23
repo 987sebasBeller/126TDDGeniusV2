a206562c25923bd00994923eb2a02ae5bbe4f86f
987sebasBeller <sebastianbeller76@gmail.com>
2023-11-22 20:51:31 -0400

    se guardan los usuarios en la memoria del navegador

--- src/presenterCrearUser.js
+++ src/presenterCrearUser.js
@@ -1,10 +1,11 @@
 import {Usuario} from "./models/Usuario";
+import { ListaUsuarios } from "./models/ListaUsuarios";
 const txtusername=document.getElementById("txtusername");
 const txtreppass=document.getElementById("txtreppass");
 const txtpassword=document.getElementById("txtpassword");
 const detalleCatalogo = document.getElementById("contenido");
 const crear = document.getElementById("btncrearuser");
-
+const usuarios=new ListaUsuarios();
 detalleCatalogo.innerHTML="";
 function verificarPasswordRepetida(usuario, repetirpassword){
     return usuario.getPassword()===repetirpassword;
@@ -15,9 +16,10 @@ function crearusuario(){
     let password=txtpassword.value;
     let repetirpassword=txtreppass.value;
     let usuario = new Usuario(username,password);
-    console.log(usuario)
     if(usuario.validarCampos() && verificarPasswordRepetida(usuario,repetirpassword)){
-        alert("Usuario creado con éxitosamente");
+      alert("Usuario creado con éxitosamente");
+      usuarios.agregarUsuario(usuario);
+      usuarios.guardarUsuarios();
     }else{
         alert("Todos los campos son obligatorios o la contraseña no es idéntica");
 
